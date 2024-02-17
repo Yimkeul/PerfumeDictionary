88d5355d10ab3210cdc9bc5c9a424c376e409023
Yimkeul <leesjun29@gmail.com>
2024-02-17 23:52:19 +0900

    Fix : add copyright for icons

--- PerfumeIntroduction.swiftpm/ContentView.swift
+++ PerfumeIntroduction.swiftpm/ContentView.swift
@@ -9,24 +9,31 @@ struct ContentView: View {
 
     var body: some View {
         NavigationSplitView {
-            List(selection: $selection) {
-                ForEach(MenuData) { index in
-                    NavigationLink(value: index.type) {
-                        Sidebar(title: index.title, desc: index.desc, image: index.image)
+                    List(selection: $selection) {
+                            ForEach(MenuData) { index in
+                                NavigationLink(value: index.type) {
+                                    Sidebar(title: index.title, desc: index.desc, image: index.image)
+                                }
+                            }
+                    }.overlay(
+                        VStack {
+                            Spacer()
+                            Text("Icons by Icons8")
+                                .foregroundColor(.Disable) // 텍스트의 색상 설정
+                                .padding() // 텍스트 주변의 여백 추가
+                        }
+                    )
+                        .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Button {
+                                selection = nil
+                            } label: {
+                                Text("Perfume Introduction")
+                                    .foregroundStyle(Color.BG)
+                                    .font(.system(size: 20, weight: .bold))
+                            }
+                        }
                     }
-                }
-            }
-                .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button {
-                        selection = nil
-                    } label: {
-                        Text("Perfume Introduction")
-                            .foregroundStyle(Color.BG)
-                            .font(.system(size: 20, weight: .bold))
-                    }
-                }
-            }
         } detail: {
             if let focus = selection {
                 MenuType.view(focus)
